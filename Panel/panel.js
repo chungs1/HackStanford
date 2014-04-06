0877e09d78f0e250cb2e19adaf974465f21a794a
jesca <jesca116@gmail.com>
2014-04-06 01:11:10 -0700

    js dynamically creating html objects (first push)

--- Panel/panel.js
+++ Panel/panel.js
@@ -1,4 +1,6 @@
- function pusher_timer() {
+/*https://github.com/pusher/real-time-progress-bar-tutorial/blob/master/public/js/home.js*/
+
+function pusher_timer() {
   var pusher = new Pusher('70931')
   var uID = p-1;
 
@@ -16,14 +18,47 @@ channel.bind('update', function(data) { // Bind to an event on our channel, in o
  }
 
 
+//task_in is Task json object (list of four)
+function generateTask(tasks_in) {
+    for (var i = 1; i<=4; i++) {
+        //change title of each subpanel
+        document.getElementById(title).innerHTML=task_in.action;
 
-function generateTask() {
-    task_id = document.getElementById("task");
-    task_id.innerHTML= " " // get task and change name
+        var title = "h2_"+i + "";
+        var curtask = tasks_in[i];
+        
+        if (curtask.type == "button") {
+            // create button
+             var new_button = $('<button />')
+                .addClass('bluebutton')
+                .text(curtask.action)
+                .mousedown(onbuttonClick()); //do something
+    }
+        
+        else if (curtask.type=="select") {
+            //create four selection buttons
+            for (var s = 1; s <=4; s++) {
+                 var new_button = $('<button />')
+                .addClass('smallbutton')
+                .text(curtask.options[i])
+                .mousedown(onselectClick()); //do something
+            }
+               
+        }
+        
+        
+        else if (curtask.type=="slider") {            
+            var new_button = $('<input />')
+                .attr({
+                    'id': 'sliderinput',
+                    'type': 'range',
+                    'min'=curtask.min_value;
+                    'max'=curtask.max_value;
+                });
+
+        }
+    }
 }
 
-function update_Title(title_num) {
-    
-}
 
 
